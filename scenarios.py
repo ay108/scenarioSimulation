394ba1a99c523209dc8f41499f69a2ee081ec47d
ay108 <146891072+ay108@users.noreply.github.com>
2024-08-07 20:02:45 -0800

    Update scenarios.py

--- scenarios.py
+++ scenarios.py
@@ -20,7 +20,7 @@ except ImportError as e:
 
 
 # Path to your data file
-data_path = 'quarter_data.xlxs'
+data_path = 'quarter_data.xlsx'
 
 # Check if the file exists
 if not os.path.isfile(data_path):
